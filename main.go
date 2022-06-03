d67cc412ea8e4744e21d197f791a34f6643eb6c7
inaba.ryo <vtryo.work@gmail.com>
2022-06-03 13:58:48 +0900

    ymlから情報を取れるようにしてみた

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/VTRyo/fakenames/helpers/jsonp"
+	"gopkg.in/yaml.v2"
 )
 
 type NameApi struct {
@@ -13,12 +15,37 @@ type NameApi struct {
 }
 
 func main() {
-	jsonStr := jsonp.Parse("https://green.adam.ne.jp/roomazi/cgi-bin/randomname.cgi?n=3")
+	url := "https://green.adam.ne.jp/roomazi/cgi-bin/randomname.cgi?n=" + readEnv()
+	jsonStr := jsonp.Parse(url)
 	var name NameApi
 	json.Unmarshal([]byte(jsonStr), &name)
 
+	// 生成数が3以外だとpanicになる模様…
 	for i := 0; i < len(name.Name[0]); i++ {
 		fmt.Printf("名前: %s(%s),\n", name.Name[i][0], name.Name[i][1])
 	}
 
+}
+
+func readEnv() string {
+	f, err := ioutil.ReadFile("./env.yml")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	yamlContent := &YamlContent{}
+	err = yaml.Unmarshal(f, &yamlContent)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//一旦Countしか返さないのでこれで…
+	return yamlContent.Generators.Count
+}
+
+type YamlContent struct {
+	Generators Generator `yaml:"generators"`
+}
+
+type Generator struct {
+	Count string `yaml:"count"`
 }
