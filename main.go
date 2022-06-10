791d102e3618940f5935ca954319c16469ef82f7
VTRyo <vtryo.work@gmail.com>
2022-06-10 23:28:52 +0900

    性格の組み合わせをランダムにする

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/VTRyo/fakenames/helpers/config"
 	"github.com/VTRyo/fakenames/helpers/jsonp"
@@ -22,8 +24,12 @@ func main() {
 	var name NameApi
 	json.Unmarshal([]byte(jsonStr), &name)
 
+	// personalitiesのリストをランダムに出力したい
+	rand.Seed(time.Now().UnixNano())
+	personalitiesLen := len(personalities)
+
 	for i := 0; i < len(name.Name); i++ {
-		fmt.Printf("名前: %s(%s), 性格: %s,\n", name.Name[i][0], name.Name[i][1], personalities[i])
+		fmt.Printf("名前: %s(%s), 性格: %s,\n", name.Name[i][0], name.Name[i][1], personalities[rand.Intn(personalitiesLen)])
 	}
 
 }
